6fce8159f153116603492d93d8c35f6b10e110ce
darthbeep <darthbeep@gmail.com>
2017-09-13 12:53:36 -0400

    merge thing

--- randomizer.py
+++ randomizer.py
@@ -1,28 +1,12 @@
 import random
-
-def print_random_name(period):
-    CLASSES = {
+CLASSES = {
         7: [ 'Helen', 'Shakil', 'Eric', 'Jennifer Y', 'Jennifer Z', 'Arif', 'Queenie', 'Jawadul', 'Shaina', 'Vivien', 'Brian', 'Naotaka', 'Bayan', 'Adam', 'Caleb', 'Terry', 'Jason', 'Alessandro', 'Samantha', 'Carol', 'Joyce', 'Shannon', 'Charles', 'Taylor', 'Kelly', 'Leo', 'Khyber', 'Ibnul', 'Eugene', 'Yuyang', 'Karina', 'Tiffany', 'Holden', 'Michael'],
         8: ['Masha', 'Adrian', 'David', 'Eric', 'Josh', 'Jerome', 'Henry', 'Jackie', 'Giorgio', 'Karen', 'Sonal', 'Xavier', 'Bermet', 'Alex', 'Iris', 'Manahal', 'Donia', 'Md', 'Connie', 'Lisa', 'Xing', 'Angelica', 'Angel', 'Augie', 'Dimitriy', 'Yiduo', 'Gordon', 'Tiffany', 'Clive', 'Jonathan', 'Sasha', 'Daniel'],
         9: [ 'Yu Qi', 'Michela', 'Kristin', 'Fabiha', 'Maxim', 'Marcus', 'Ish', 'James', 'Ryan', 'Edward', 'Adeeb', 'Jake', 'Cynthia', 'Kevin', 'Levi', 'Edmond', 'Kyle', 'Andrew', 'Max', 'Jenny', 'Philip', 'Shan', 'Mansour', 'Ray', 'Jake', 'Ida', 'Kerry', 'Stanley', 'Jackie', 'William', 'Tina', 'Michael']
 }
-    if period not in [7, 8, 9]:
-        print "that period has no students"
-        return
-    num = random.randint(0, len(CLASSES[period]) + 1)
-    print CLASSES[period][num]
+period = input("enter your period ")
 
-def main():
-    while True:
-        print "Input a period [7, 8, 9] or exit: "
-        per = raw_input()
-        if per == "exit":
-            break
-        try:
-            per = int(per)
-        except: 
-            print "Please input a number"
-            continue
-        print_random_name(per)
-
-main()
+try:
+	print CLASSES[period][int(random.random() * len(CLASSES[period]))]
+except:
+	print "no thats not a period"
